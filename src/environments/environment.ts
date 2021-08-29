02d98d68b08126a3fc988402718a8ef592b888ba
sandun.m <sandun.madampage@scicom.com.my>
2021-08-29 16:53:33 +0530

    depl: heroku deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyCDHrQasHNc9q11grA2cBOCjl5YrlaKelc",
     authDomain: "tutorx-platform.firebaseapp.com",
